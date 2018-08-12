f84e346dfb0469167d787454e86c0599da573cb4
Saib Ahmed <saibahmed1512@gmail.com>
2018-08-12 10:34:32 +0530

    commit

--- App.js
+++ App.js
@@ -6,14 +6,22 @@ var budgetController = (function(){
 
 //UIController Controller
 var UIController = (function(){ 
-    
+    return{
+      getInput:function(){
+          return{
+           type: document.querySelector(".add__type").value,//will be either inc or exp
+           desciption : document.querySelector(".add__description").value,
+           value : document.querySelector("add__value").value;    
+          };
+     }  
+    };
 })();
 
 //Global App Controller
 var Controller = (function (budgetctrl,UIctrl){
  var ctrlAddItem = function(){
             console.log("enter was pressed")
-        //todo
+        //todo 
 //        1.get the input data
 //        2.add the item to the budget controller
 //        3.add the new item to the function
@@ -21,6 +29,7 @@ var Controller = (function (budgetctrl,UIctrl){
 //        5.display the budget on the UI
         }
     document.querySelector(".add__btn").addEventListener("clicked", function(){
+        console.log("Button was clicked");
 });
      
     document.addEventListener("keypress", function(event){
